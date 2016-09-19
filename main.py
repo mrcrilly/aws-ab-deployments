cb39a78010183f8c05632514f568bf97b94bd97c
Michael Crilly <mc@sent.com>
2016-09-19 13:07:45 +1000

    allowing for a check for clean cluster status; adding verbosity/logging

--- main.py
+++ main.py
@@ -14,51 +14,12 @@ def check_error(err):
         print err 
         sys.exit(-999)
 
-# def current_instance_count(group_name):
-#     pass
-
-
-# def check_instance_health(instance_id):
-#     pass
-
-
-# def check_backend_health(elb_name, instance_id):
-#     pass
-
-# def reap_and_replace_instances(args, asg_name, instances_to_reap):
-#   activity_id = asg.detach_instances(InstanceIds=instances_to_reap, AutoScalingGroupName=asg_name, ShouldDecrementDesiredCapacity=True)["Activities"][0]["ActivityId"]
-#   while(asg.describe_scaling_activities(ActivityIds=[activity_id], AutoScalingGroupName=asg_name, MaxRecords=1)["Activities"]["Progress"] != 100):
-#       time.sleep(args.update_timeout/2)
-
-
-
-# def nuture_instances_to_health(args, asg_name, new_instances):
-#   elb_health = False
-#     instances_to_reap = []
-#     health_check_start = time.time()
-#     while (not elb_health):
-#         print "Have instance IDs, checking their health in the ELB (%s)..." % args.elb_name
-#         time.sleep(args.update_timeout)
-
-#         elb_instances = elb.describe_instance_health(LoadBalancerName=args.elb_name, Instances=new_instances)
-#         for instance in elb_instances["InstanceStates"]:
-#           if instance["InstanceId"] in instances_to_reap:
-#               continue 
-
-#             if instance["State"] == "InService":
-#                 elb_health = True
-#             else:
-#                 elb_health = False
-
-#                 if int(math.ceil(time.time() - health_check_start)) >= args.health_check_timeout:
-#                   instances_to_reap.append(instance["InstanceId"])
-
-#     if not len(instances_to_reap) == 0:
-#       reap_and_replace_instances(args, asg_name, instances_to_reap)
-
-#     asg.set_desired_capacity(AutoScalingGroupName=asg_name, DesiredCapacity=0)
+def if_verbose(message):
+    if args.verbose:
+        logging.info(message)
 
 def scale_up_autoscaling_group(asg_name, instance_count):
+    if_verbose("Scaling up ASG %s by %d instances" % (asg_name, instance_count))
     asg.set_desired_capacity(AutoScalingGroupName=asg_name, DesiredCapacity=instance_count)
     activities = asg.describe_scaling_activities(AutoScalingGroupName=asg_name, MaxRecords=args.instance_count_step)
     activity_ids = [a["ActivityId"] for a in activities["Activities"]]
@@ -66,6 +27,7 @@ def scale_up_autoscaling_group(asg_name, instance_count):
     if not len(activity_ids) > 0:
         return "No activities found"        
     
+    if_verbose("Activities found, checking them until complete or %d tmer expires" % args.health_check_timeout)
     activities_are_incomplete = True
     timer = time.time()
     while(activities_are_incomplete):
@@ -81,9 +43,11 @@ def scale_up_autoscaling_group(asg_name, instance_count):
             else:
                 activities_are_incomplete = True
 
+    if_verbose("Scaling up of ASG %s successful" % asg_name)
     return None
 
 def check_autoscaling_group_health(asg_name):
+    if_verbose("Checking the health of ASG %s" % asg_name)
     asg_is_not_healthy = True
     timer = time.time()
     while(asg_is_not_healthy):
@@ -99,9 +63,11 @@ def check_autoscaling_group_health(asg_name):
         if int(time.time() - timer) >= args.health_check_timeout:
             return "Health check timer expired on asg_instances count. A manual clean up is likely."
 
+    if_verbose("ASG %s is healthy" % asg_name)
     return None
 
 def check_elb_instance_health(elb_name, instances):
+    if_verbose("Checking ELB %s instance health for %s" % (elb_name, instances))
     elb_is_unhealthy = True
     timer = time.time()
     while (elb_is_unhealthy):
@@ -117,6 +83,7 @@ def check_elb_instance_health(elb_name, instances):
         if int(time.time() - timer) >= args.health_check_timeout:
             return "Health check timer expired. A manual clean up is likely."
 
+    if_verbose("ELB %s is healthy with instances %s" (elb_name, instances))
     return None
 
 def scale_up_application(up, down):
@@ -124,12 +91,14 @@ def scale_up_application(up, down):
     asg_instances = []
     asg_health = False
 
+    if_verbose("Scaling up %s by steps of %d", % (asg_name, args.instance_count_step))
     asg_instances = asg.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name], MaxRecords=1)["AutoScalingGroups"][0]["Instances"]
     current_capacity_count = args.instance_count_step
 
     if len(asg_instances) >= 1:
         check_error("Failure. There are instances inside the target ASG: %s" % up)
 
+    if_verbose("Entering scale_up_application loop until new ASG instances are up")
     we_have_not_deployed = True
     while(we_have_not_deployed):
         check_error(scale_up_autoscaling_group(asg_name, current_capacity_count))
@@ -143,15 +112,40 @@ def scale_up_application(up, down):
         else:
             current_capacity_count += args.instance_count_step
 
+    if_verbose("Scaling up %s successful" % asg_name)
+
+def ensure_clean_cluster(asg_name, elb_name):
+    if_verbose("Ensuring %s is a clean/healthy cluster" % asg_name)
+    current_instances = elb.describe_load_balancers(LoadBalancerNames[elb_name])["LoadBalancerDescriptions"][0]["Instances"]
+    if len(current_instances) == 0:
+        if_verbose("No instances found in %s. Skipping." % asg_name)
+        return None
+
+    if_verbose("Instances found, checking their ELB status")
+    current_state = elb.describe_instance_health(LoadBalancerName=elb_name, Instances=current_instances)["InstanceStates"]
+    if len(current_state) == 0:
+        return "Unable to fetch ELB state"
+
+    for instance in current_state:
+        if instance["State"] != "InService":
+            return "ELB status is unclean. Manual clean up required."
+
+    if_verbose("ELB status is clean")
+    return None
+
 def scale_down_application(asg_name):
+    if_verbose("Scaling down %s." % asg_name)
     asg.set_desired_capacity(AutoScalingGroupName=asg_name, DesiredCapacity=0)
 
 def main():
     environment_a = asg.describe_auto_scaling_groups(AutoScalingGroupNames=["%s-a" % args.environment], MaxRecords=1)
     environment_b = asg.describe_auto_scaling_groups(AutoScalingGroupNames=["%s-b" % args.environment], MaxRecords=1)
 
+    if_verbose("I have AutoScaling Groups: %s and %s" % ("%s-a" % args.environment, "%s-b" % args.environment))
+
     if (environment_a["AutoScalingGroups"][0]["DesiredCapacity"] == 0) and (environment_b["AutoScalingGroups"][0]["DesiredCapacity"] == 0):
-        print "No active ASG; starting with %s-a" % args.environment
+        logging.info("No active ASG; starting with %s-a" % args.environment)
+
         if not args.dryrun:
             scale_up_application("a", "b")
             scale_down_application("%s-b"%args.environment)
@@ -160,39 +154,35 @@ def main():
         check_error("Failure. Unable to find an ASG that is empty. Both contain instances.")
 
     elif environment_a["AutoScalingGroups"][0]["DesiredCapacity"] > 0:
-        print "Currently active ASG is %s-a; bringing up %s-b" % (args.environment, args.environment)
+        logging.info("Currently active ASG is %s-a; bringing up %s-b" % (args.environment, args.environment))
+
         if not args.dryrun:
+            check_error(ensure_clean_cluster(args.elb_name))
             scale_up_application("b", "a")
             scale_down_application("%s-a"%args.environment)
 
     elif environment_b["AutoScalingGroups"][0]["DesiredCapacity"] > 0:
-        print "Currently active ASG is %s-b; bringing up %s-a" % (args.environment, args.environment)
+        logging.info("Currently active ASG is %s-b; bringing up %s-a" % (args.environment, args.environment))
+
         if not args.dryrun:
+            check_error(ensure_clean_cluster(args.elb_name))
             scale_up_application("a", "b")
             scale_down_application("%s-b"%args.environment)
-
 
 if __name__ == "__main__":
     global parser
     global args 
 
     parser = argparse.ArgumentParser(description='Build Data to S3')
-    parser.add_argument("--dry-run", dest="dryrun", help="Only detect what we would do; don't run anything",
-                        action='store_true', required=False)
-    parser.add_argument("--environment", dest="environment", help="The environment to A/B deploy against",
-                        required=True)
+    parser.add_argument("--dry-run", dest="dryrun", help="Only detect what we would do; don't run anything", action='store_true', required=False)
+    parser.add_argument("--environment", dest="environment", help="The environment to A/B deploy against", required=True)
     parser.add_argument("--elb-name", dest="elb_name", help="The ELB to which your ASG is linked", required=True)
-    parser.add_argument("--instance-count", dest="instance_count",
-                        help="How many instances you want tho ASG to grow by (default: 2)", required=False, type=int,
-                        default=2)
+    parser.add_argument("--instance-count", dest="instance_count", help="How many instances you want tho ASG to grow by (default: 2)", required=False, type=int, default=2)
     parser.add_argument("--instance-count-step", dest="instance_count_step", help="How many instances to scale by at a time (default: 2)", required=False, type=int, default=2)
-    parser.add_argument("--update-timeout", dest="update_timeout",
-                        help="How long to wait between API calls/console updates (default: 30s)", required=False,
-                        type=int, default=5)
-    parser.add_argument("--health-check-timeout", dest="health_check_timeout",
-                        help="How long to wait for the health of an ELB to stabilse (default: 600s/10m)", required=False,
-                        type=int, default=600)
+    parser.add_argument("--update-timeout", dest="update_timeout", help="How long to wait between API calls/console updates (default: 30s)", required=False, type=int, default=5)
+    parser.add_argument("--health-check-timeout", dest="health_check_timeout", help="How long to wait for the health of an ELB to stabilse (default: 600s/10m)", required=False, type=int, default=600)
     parser.add_argument("--clean-up", dest="clean_up", help="Clean up existing ASGs if they ahve instances. Very dangerous option! (default: false)", required=False, default=False)
+    parser.add_argument("--verbose", dest="verbose", help="Print messages about progress and what step we're at (default: false)", required=False, default=False)
     args = parser.parse_args()
 
     sys.exit(main())
